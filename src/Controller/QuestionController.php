f09db859e4138d1c08e0730ae8776cf9cb3cbef5
Chris G <chris@campaignrep.com>
2020-11-23 10:52:40 -0800

    new

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -4,8 +4,10 @@
 
 namespace App\Controller;
 
+use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Cache\CacheInterface;
 
 class QuestionController extends AbstractController
 {
@@ -15,6 +17,13 @@ class QuestionController extends AbstractController
      */
     public function homepage()
     {
+
+        /*
+        $html = $twigEnviroment->render('question/homepage.html.twig');
+        return new Response($html);
+        */
+
+
         // return new Response('What a bewithcing controller we have whipped up here!');
         return $this->render(
             'question/homepage.html.twig'
@@ -24,23 +33,34 @@ class QuestionController extends AbstractController
     /**
      * @Route("/questions/{slug}", name="app_question_show")
      */
-    public function show($slug)
+    public function show($slug, MarkdownParserInterface $markdownParser, CacheInterface $cache)
     {
         // return new Response(sprintf(
         //     'This is the URL Title Maybe: %s', 
         //     ucwords(str_replace('-', ' ', $slug))
         // ));
 
+        $questionText = 'I\'ve been turned into a cat, any *thoughts* on how to turn back? While I\'m **adorable**, I don\'t really care for cat food.';
+        // $parsedQuestionText = $markdownParser->transformMarkdown($questionText);
+
+        $parsedQuestionText = $cache->get('markdown_'.md5($questionText), function() use($markdownParser, $questionText) {
+            return $markdownParser->transformMarkdown($questionText);
+        });
+
+        dd($markdownParser);
+
+
         $answers = [
-            'Make sure your cat is sitting purrrfectly still 🤣',
+            'Make sure your cat is sitting `purrrfectly` still 🤣',
             'Honestly, I like furry shoes better than MY cat',
             'Maybe... try saying the spell backwards?',
         ];
 
-        dump($slug, $this);
+        // dump($slug, $this);
 
         return $this->render('question/show.html.twig', [
             'question' => ucwords(str_replace('-', ' ', $slug)),
+            'questionText' => $parsedQuestionText,
             'answers' => $answers,
         ]);
     }
